63206d8112093d2791e7e76e57f029eebb93225b
vpinedo-neostella <victor.pinedo@neostella.com>
2024-03-27 12:48:16 -0500

    feat(setup): add firebase

---
+++ firebaseConfig.tsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDd6cq_TQiTBvZr--Azdj3XXNbFgjTyWgA",
+  authDomain: "loan-tracker-14253.firebaseapp.com",
+  projectId: "loan-tracker-14253",
+  storageBucket: "loan-tracker-14253.appspot.com",
+  messagingSenderId: "428250568481",
+  appId: "1:428250568481:web:e4a1600f5f57177f9b5060",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
