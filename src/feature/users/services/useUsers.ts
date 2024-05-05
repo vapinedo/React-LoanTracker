2c16988cc0397cb1eb20299d35884f3df1ad60d0
Victor Pinedo <victorpinedomartinez@gmail.com>
2024-05-05 12:43:02 -0500

    feat(users): update users service

--- src/feature/users/services/useUsers.ts
+++ src/feature/users/services/useUsers.ts
@@ -1,12 +1,30 @@
 import appFirebase from "@firebaseConfig";
-import { getAuth, signOut } from "firebase/auth";
+import { getAuth, signOut, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
 
 const AUTH_DATA = getAuth(appFirebase);
 
 export default function useUsers() {
-  const logout = async () => {
-    await signOut(AUTH_DATA);
-  }
 
-  return { logout };
+    const create = async (email: string, passsword: string) => {
+        try {
+            const user = await createUserWithEmailAndPassword(AUTH_DATA, email, passsword);
+            console.log({user});
+        } catch (error) {
+            console.log("Error al hacer login", error);
+        }
+    };
+
+    const login = async (email: string, password: string) => {
+        try {
+            await signInWithEmailAndPassword(AUTH_DATA, email, password);
+        } catch (error) {
+            console.log("Error al hacer login", error);
+        }
+    };
+
+    const logout = async () => {
+        await signOut(AUTH_DATA);
+    }
+
+    return { create, login, logout };
 }
