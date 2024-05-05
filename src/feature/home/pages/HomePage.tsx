c0b0b018d1cad8e1cc4eacb4bba3d32ba040eda2
Victor Pinedo <victorpinedomartinez@gmail.com>
2024-05-05 13:58:33 -0500

    feat(users): add formik

--- src/feature/home/pages/HomePage.tsx
+++ src/feature/home/pages/HomePage.tsx
@@ -1,13 +1,32 @@
-import { Box, Button } from '@mui/material';
+
+import { Form, Formik } from 'formik';
+import { Button } from '@mui/material';
 import TextField from '@mui/material/TextField';
+
+interface User {
+    nombres: string;
+    apellidos: string;
+    correo: string;
+    celular: string;
+    direccion: string;
+    password: string;
+    password_confirm: string;
+}
+
+const initialValues: User = {
+    nombres: "",
+    apellidos: "",
+    correo: "",
+    celular: "",
+    direccion: "",
+    password: "",
+    password_confirm: "",
+};
 
 export default function HomePage() {
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        // const { nombres, apellidos, correo, celular, direccion, password, password_confirm } = data;
-        console.log({ data });
+    const handleSubmit = (values: any, setSubmitting: any) => {
+        
     };
 
     return (
@@ -17,45 +36,48 @@ export default function HomePage() {
                 <button className="btn btn-outline-danger">Volver Atrás</button>
             </header>
 
-            <Box component="form" noValidate onSubmit={handleSubmit}>
-                <div className="row">
-                    <div className="col-md-8 mb-3">
-                        <TextField fullWidth name="nombres" id="nombres" label="Nombres" size='small' variant="outlined" />
+            <Formik initialValues={initialValues} onSubmit={(values, { setSubmitting }) => handleSubmit(values, setSubmitting)}>
+                <Form>
+                    <div className="row">
+                        <div className="col-md-8 mb-3">
+                            <TextField fullWidth name="nombres" id="nombres" label="Nombres" size='small' variant="outlined" />
+                        </div>
+
+                        <div className="col-md-8 mb-3">
+                            <TextField fullWidth name="apellidos" id="apellidos" label="Apellidos" size='small' variant="outlined" />
+                        </div>
+
+                        <div className="col-md-8 mb-3">
+                            <TextField fullWidth name="correo" id="correo" label="Correo" size='small' variant="outlined" />
+                        </div>
+
+                        <div className="col-md-8 mb-3">
+                            <TextField fullWidth name="celular" id="celular" label="Celular" size='small' variant="outlined" />
+                        </div>
+
+                        <div className="col-md-8 mb-3">
+                            <TextField fullWidth name="direccion" id="direccion" label="Dirección" size='small' variant="outlined" />
+                        </div>
+
+                        <div className="col-md-8 mb-3">
+                            <TextField fullWidth name="password" id="pasword" label="Contraseña" size='small' variant="outlined" />
+                        </div>
+
+                        <div className="col-md-8 mb-3">
+                            <TextField fullWidth name="password_confirm" id="password_confirm" label="Repetir contraseña" size='small' variant="outlined" />
+                        </div>
+
                     </div>
 
-                    <div className="col-md-8 mb-3">
-                        <TextField fullWidth name="apellidos" id="apellidos" label="Apellidos" size='small' variant="outlined" />
-                    </div>
-
-                    <div className="col-md-8 mb-3">
-                        <TextField fullWidth name="correo" id="correo" label="Correo" size='small' variant="outlined" />
-                    </div>
-
-                    <div className="col-md-8 mb-3">
-                        <TextField fullWidth name="celular" id="celular" label="Celular" size='small' variant="outlined" />
-                    </div>
-
-                    <div className="col-md-8 mb-3">
-                        <TextField fullWidth name="direccion" id="direccion" label="Dirección" size='small' variant="outlined" />
-                    </div>
-
-                    <div className="col-md-8 mb-3">
-                        <TextField fullWidth name="password" id="pasword" label="Contraseña" size='small' variant="outlined" />
-                    </div>
-
-                    <div className="col-md-8 mb-3">
-                        <TextField fullWidth name="password_confirm" id="password_confirm" label="Repetir contraseña" size='small' variant="outlined" />
-                    </div>
-                </div>
-
-                <Button
-                    type="submit"
-                    variant="contained"
-                    sx={{ mt: 3, mb: 2 }}
-                >
-                    Crear usuario
-                </Button>
-            </Box>
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        sx={{ marginTop: 2 }}
+                    >
+                        Crear usuario
+                    </Button>
+                </Form>
+            </Formik>
         </div>
     )
 }
