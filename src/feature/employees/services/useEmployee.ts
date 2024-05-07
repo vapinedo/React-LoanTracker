7f030819a17e5b2a2d96a42875e267516233a78b
Victor Pinedo <victorpinedomartinez@gmail.com>
2024-05-07 16:10:58 -0500

    feat(sweetalert): done and added delete employee

--- src/feature/employees/services/useEmployee.ts
+++ src/feature/employees/services/useEmployee.ts
@@ -1,8 +1,8 @@
+import toast from 'react-hot-toast';
 import { v4 as createUuid } from 'uuid';
 import firebaseApp from "@firebaseConfig";
 import { Employee } from "../models/Employee";
-import { doc, getDocs, getDoc, setDoc, collection, getFirestore, runTransaction } from "firebase/firestore";
-import toast from 'react-hot-toast';
+import { doc, getDocs, getDoc, setDoc, collection, getFirestore, runTransaction, deleteDoc } from "firebase/firestore";
 
 const COLLECTION = "EMPLOYEES";
 const db = getFirestore(firebaseApp);
@@ -10,30 +10,30 @@ const db = getFirestore(firebaseApp);
 export default function useEmployee() {
 
     const getAllEmployees = async () => {
-        const employees: any[] = [];
+        const documents: any[] = [];
         try {
             const querySnapshot = await getDocs(collection(db, COLLECTION));
             querySnapshot.forEach((doc) => {
-                employees.push(doc.data());
+                documents.push(doc.data());
             });
         } catch (error) {
             console.log(error);
         }
-        return employees;
+        return documents;
     };
 
     const getEmployeeById = async (documentId: string) => {
-        let employee = null;
+        let document = null;
         try {
             const docRef = doc(db, COLLECTION, documentId);
             const docSnap = await getDoc(docRef);
             if (docSnap.exists()) {
-                employee = docSnap.data();
+                document = docSnap.data();
             }
         } catch (error) {
             console.log(error);
         }
-        return employee;
+        return document;
     };
 
     const createEmployee = async (document: Employee) => {
@@ -41,6 +41,7 @@ export default function useEmployee() {
             const documentId = createUuid();
             document.id = documentId;
             await setDoc(doc(db, COLLECTION, documentId), document);
+            toast.success("Empleado creado exitosamente!");
         } catch (error) {
             console.log(error);
         }
@@ -61,11 +62,23 @@ export default function useEmployee() {
             console.error(error);
         }
     };
+    
+    const deleteEmployee = async (documentId: string) => {
+        try {
+            const docRef = doc(db, COLLECTION, documentId);
+            const response = await deleteDoc(docRef);
+            console.log({response});
+            toast.success("Empleado eliminado exitosamente!");
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
     return {
         getAllEmployees,
         getEmployeeById,
         createEmployee,
-        updateEmployee
+        updateEmployee,
+        deleteEmployee,
     };
 }
