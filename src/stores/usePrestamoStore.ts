563c649d1aefa3f31d55f8a87b4d4d745abad18d
Victor Pinedo <victorpinedomartinez@gmail.com>
2024-05-17 14:46:51 -0500

    se agrega persistencia

--- src/stores/usePrestamoStore.ts
+++ src/stores/usePrestamoStore.ts
@@ -1,6 +1,7 @@
 import { create } from 'zustand';
 import usePrestamos from '@services/usePrestamos';
 import { Prestamo } from '@features/prestamos/models/Prestamo';
+import { persist, createJSONStorage } from 'zustand/middleware';
 
 interface PrestamoStore {
     prestamos: Prestamo[];
@@ -13,73 +14,82 @@ interface PrestamoStore {
     deletePrestamo: (id: string) => Promise<void>;
 }
 
-// Create the store
-const usePrestamoStore = create<PrestamoStore>((set, get) => ({
-    prestamos: [],
-    loading: false,
-    error: null,
+const usePrestamoStore = create<PrestamoStore>()(
+    persist(
+        (set, get) => ({
+            prestamos: [],
+            loading: false,
+            error: null,
 
-    fetchPrestamos: async () => {
-        set({ loading: true, error: null });
-        try {
-            const prestamos = await usePrestamos().getAllPrestamos();
-            set({ prestamos, loading: false });
-        } catch (error: unknown) {
-            if (error instanceof Error) {
-                set({ error: error.message, loading: false });
-            } else {
-                set({ error: String(error), loading: false });
-            }
+            fetchPrestamos: async () => {
+                set({ loading: true, error: null });
+                try {
+                    const prestamos = await usePrestamos().getAllPrestamos();
+                    set({ prestamos, loading: false });
+                } catch (error: unknown) {
+                    if (error instanceof Error) {
+                        set({ error: error.message, loading: false });
+                    } else {
+                        set({ error: String(error), loading: false });
+                    }
+                }
+            },
+
+            getPrestamo: (id: string) => {
+                const { prestamos } = get();
+                return prestamos.find(prestamo => prestamo.id === id);
+            },
+
+            createPrestamo: async (prestamo: Prestamo) => {
+                set({ loading: true, error: null });
+                try {
+                    await usePrestamos().createPrestamo(prestamo);
+                    // Fetch the updated list of prestamos
+                    await get().fetchPrestamos();
+                } catch (error: unknown) {
+                    if (error instanceof Error) {
+                        set({ error: error.message, loading: false });
+                    } else {
+                        set({ error: String(error), loading: false });
+                    }
+                }
+            },
+
+            updatePrestamo: async (prestamo: Prestamo) => {
+                set({ loading: true, error: null });
+                try {
+                    await usePrestamos().updatePrestamo(prestamo);
+                    // Fetch the updated list of prestamos
+                    await get().fetchPrestamos();
+                } catch (error: unknown) {
+                    if (error instanceof Error) {
+                        set({ error: error.message, loading: false });
+                    } else {
+                        set({ error: String(error), loading: false });
+                    }
+                }
+            },
+
+            deletePrestamo: async (id: string) => {
+                set({ loading: true, error: null });
+                try {
+                    await usePrestamos().deletePrestamo(id);
+                    // Fetch the updated list of prestamos
+                    await get().fetchPrestamos();
+                } catch (error: unknown) {
+                    if (error instanceof Error) {
+                        set({ error: error.message, loading: false });
+                    } else {
+                        set({ error: String(error), loading: false });
+                    }
+                }
+            },
+        }),
+        {
+            name: "prestamos-store",
+            storage: createJSONStorage(() => sessionStorage),
         }
-    },
-
-    getPrestamo: (id: string) => {
-        const { prestamos } = get();
-        return prestamos.find(prestamo => prestamo.id === id);
-    },
-
-    createPrestamo: async (prestamo: Prestamo) => {
-        set({ loading: true, error: null });
-        try {
-            await usePrestamos().createPrestamo(prestamo);
-        } catch (error: unknown) {
-            if (error instanceof Error) {
-                set({ error: error.message, loading: false });
-            } else {
-                set({ error: String(error), loading: false });
-            }
-        }
-    },
-
-    updatePrestamo: async (prestamo: Prestamo) => {
-        set({ loading: true, error: null });
-        try {
-            await usePrestamos().updatePrestamo(prestamo);
-            // Fetch the updated list of prestamos
-            await get().fetchPrestamos();
-        } catch (error: unknown) {
-            if (error instanceof Error) {
-                set({ error: error.message, loading: false });
-            } else {
-                set({ error: String(error), loading: false });
-            }
-        }
-    },
-
-    deletePrestamo: async (id: string) => {
-        set({ loading: true, error: null });
-        try {
-            await usePrestamos().deletePrestamo(id);
-            // Fetch the updated list of prestamos
-            await get().fetchPrestamos();
-        } catch (error: unknown) {
-            if (error instanceof Error) {
-                set({ error: error.message, loading: false });
-            } else {
-                set({ error: String(error), loading: false });
-            }
-        }
-    },
-}));
+    )
+);
 
 export default usePrestamoStore;
