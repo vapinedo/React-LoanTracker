7dfc7b59cd038610c2ce320fea492feefcda529f
Victor Pinedo <victorpinedomartinez@gmail.com>
2024-05-17 15:31:09 -0500

    Refactor aplicado

--- src/components/form/CustomTextField.tsx
+++ src/components/form/CustomTextField.tsx
@@ -1,30 +1,26 @@
-import { TextField } from "@mui/material";
-import { InputHTMLAttributes } from "react";
+import { FC } from "react";
+import { TextField, TextFieldProps } from "@mui/material";
+import { UseFormRegisterReturn } from "react-hook-form";
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+interface InputProps extends Omit<TextFieldProps, 'error' | 'name'> {
     name: string;
-    register?: any;
-    label?: string;
+    register?: UseFormRegisterReturn;
     error?: string;
 }
 
-export default function CustomTextField(props: InputProps): any {
-    const {
-        name,
-        error,
-        label,
-        register,
-        ...rest
-    } = props;
-
+const CustomTextField: FC<InputProps> = ({ name, error, register, ...rest }) => {
     return (
         <TextField
             {...rest}
-            label={label}
-            {...register(name)}
+            label={rest.label}
+            {...(register ? register : {})}
+            name={name}
             sx={{ width: '100%' }}
             InputLabelProps={{ shrink: true }}
-            helperText={<span className="text-danger">{error}</span>}
+            helperText={error && <span className="text-danger">{error}</span>}
+            error={!!error}
         />
     );
 }
+
+export default CustomTextField;
