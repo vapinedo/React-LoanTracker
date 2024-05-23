50e493f7919f52267e28a4a72f7713e851ebb000
Victor Pinedo <victorpinedomartinez@gmail.com>
2024-05-23 15:45:29 -0500

    transiciones completadas

--- src/layouts/BoxShadow.tsx
+++ src/layouts/BoxShadow.tsx
@@ -1,5 +1,7 @@
+import 'animate.css';
+
 export default function BoxShadow({children}) {
   return (
-    <div className="box-shadow-layout">{children}</div>
+    <div className="box-shadow-layout animate__animated animate__fadeIn animate_faster">{children}</div>
   )
 }
