027d86eabdba0ddbb22fda6386f2885700812765
Victor Pinedo <victorpinedomartinez@gmail.com>
2024-05-23 08:17:15 -0500

    fix completado

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "@components/Navbar";
 import useAuth from "@services/useAuth";
 import { Toaster } from "react-hot-toast";
@@ -6,24 +6,32 @@ import { Route, Routes } from "react-router-dom";
 import Breadcrumbs from "@components/Breadcrumbs";
 import PrivateRoute from "@app/routes/PrivateRoute";
 import NotFoundPage from "@components/NotFoundPage";
+import HomePage from "@features/home/pages/HomePage";
 import LoginPage from "@features/auth/pages/LoginPage";
 import ClientesRouter from "@app/features/clientes/ClientesRouter";
 import PrestamosRouter from "@app/features/prestamos/PrestamosRouter";
 import EmpleadosRouter from "@app/features/empleados/EmpleadosRouter";
 
 export default function AppRouter() {
-
     const { user } = useAuth();
-    
+    const [isAuthChecked, setIsAuthChecked] = useState(false);
+
+    useEffect(() => {
+        setIsAuthChecked(true);
+    }, [user]);
+
     return (
         <React.Fragment>
-            {user && <Navbar />}
+            {isAuthChecked && user && <Navbar /> }
 
             <section className="container mt-4 mb-5 px-5">
-                { user && <Toaster />}
-                { user && <Breadcrumbs />}
+                {isAuthChecked && user && <Toaster />}
+                {isAuthChecked && user &&  <Breadcrumbs />}
+                
                 <Routes>
-                    <Route path="/" element={ <LoginPage /> } />
+                    <Route path="/login" element={<LoginPage />} />
+                    
+                    <Route path="/" element={<PrivateRoute Component={HomePage} />} />
                     <Route path="/clientes/*" element={<PrivateRoute Component={ClientesRouter} />} />
                     <Route path="/empleados/*" element={<PrivateRoute Component={EmpleadosRouter} />} />
                     <Route path="/prestamos/*" element={<PrivateRoute Component={PrestamosRouter} />} />
@@ -31,5 +39,5 @@ export default function AppRouter() {
                 </Routes>
             </section>
         </React.Fragment>
-    )
+    );
 }
