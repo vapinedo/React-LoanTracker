0e4dfc635d10ce446ec5bdde7166baf3def821f6
Victor Pinedo <victorpinedomartinez@gmail.com>
2024-05-29 11:11:39 -0500

    ajuste estetico

--- src/features/dashboard/pages/DashboardPage.tsx
+++ src/features/dashboard/pages/DashboardPage.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
-import useDashboardStore from "@stores/useDashboardStore";
 import IconSVG from "@components/IconSVG";
+import useDashboardStore from "@stores/useDashboardStore";
 import MiniCard from "@features/dashboard/components/MiniCard";
 
 export default function DashboardPage() {
@@ -15,25 +15,25 @@ export default function DashboardPage() {
             <div className="row">
                 <div className="col-md-4">
                     <MiniCard
-                        data={loading ? '...' : error ? 'Error' : totalClientes}
                         title="Clientes"
                         icon={<IconSVG width="32" height="32" />}
+                        data={loading ? '...' : error ? 'Error' : totalClientes}
                     />
                 </div>
 
                 <div className="col-md-4">
                     <MiniCard
-                        data={loading ? '...' : error ? 'Error' : totalEmpleados}
                         title="Empleados"
                         icon={<IconSVG width="32" height="32" />}
+                        data={loading ? '...' : error ? 'Error' : totalEmpleados}
                     />
                 </div>
 
                 <div className="col-md-4">
                     <MiniCard
-                        data={loading ? '...' : error ? 'Error' : totalPrestamos}
                         title="Prestamos"
                         icon={<IconSVG width="32" height="32" />}
+                        data={loading ? '...' : error ? 'Error' : totalPrestamos}
                     />
                 </div>
             </div>
