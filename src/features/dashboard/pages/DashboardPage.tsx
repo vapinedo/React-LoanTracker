8ccd81b00b10ee73dc159dacfb0d41bfb7cd56d6
Victor Pinedo <victorpinedomartinez@gmail.com>
2024-05-29 20:33:12 -0500

    agregar grafico de linea, basico con recharts

--- src/features/dashboard/pages/DashboardPage.tsx
+++ src/features/dashboard/pages/DashboardPage.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import IconSVG from "@components/IconSVG";
 import useDashboardStore from "@stores/useDashboardStore";
 import MiniCard from "@features/dashboard/components/MiniCard";
+import CustomLineChart from "@features/dashboard/components/CustomLineChart";
 
 export default function DashboardPage() {
     const { totalClientes, totalEmpleados, totalPrestamos, loading, error, fetchTotals } = useDashboardStore();
@@ -35,6 +36,12 @@ export default function DashboardPage() {
                         icon={<IconSVG width="32" height="32" />}
                         data={loading ? '...' : error ? 'Error' : totalPrestamos}
                     />
+                </div>
+            </div>
+
+            <div className="row mt-4">
+                <div className="col-md-6">
+                    <CustomLineChart />
                 </div>
             </div>
         </section>
